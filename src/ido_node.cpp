99bc6574d02b704f75ee51a43f4edfc324b87fd9
Leonard Bruns <leonardb@kth.se>
2023-03-19 13:49:58 +0100

    Add simplified node skeleton from tutorials

---
+++ src/ido_node.cpp
@@ -0,0 +1,45 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+
+// TODO make class for node
+
+void scanCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "talker");
+
+  ros::NodeHandle n;
+
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+  ros::Subscriber sub = n.subscribe("scan", 1000, scanCallback);
+
+  ros::Rate loop_rate(10);
+
+  ros::spin();
+  /* int count = 0; */
+  /* while (ros::ok()) { */
+  /*   std_msgs::String msg; */
+
+  /*   std::stringstream ss; */
+  /*   ss << "hello world " << count; */
+  /*   msg.data = ss.str(); */
+
+  /*   ROS_INFO("%s", msg.data.c_str()); */
+
+  /*   chatter_pub.publish(msg); */
+
+  /*   ros::spinOnce(); */
+
+  /*   loop_rate.sleep(); */
+  /*   ++count; */
+  /* } */
+
+  return 0;
+}
