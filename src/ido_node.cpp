462d92c6d3616f1cf914f444631d4f37eac77c66
Leonard Bruns <leonardb@kth.se>
2023-03-20 00:31:37 +0100

    Add outline for code, define ProbabilityGrid struct

--- src/ido_node.cpp
+++ src/ido_node.cpp
@@ -6,10 +6,28 @@
 
 // TODO make class instead of globals
 nav_msgs::OccupancyGrid occupancy_grid;
-const size_t WIDTH = 100;
-const size_t HEIGHT = 100;
-const double CELL_SIZE = 0.05;
-const int8_t INIT_VALUE = 10; // -128 == 0.0; 127 == 1.0
+const double WIDTH = 7;  // width of map in meters
+const double HEIGHT = 7;  // height of map in meters
+const size_t RESOLUTION = 15;  // cells per meter
+const double PRIOR_PROB = 0.5;
+
+struct ProbabilityGrid {
+  ProbabilityGrid(size_t width, size_t height, double a=0.5)
+    : probabilities(width * height, a), width(width), height(height) {}
+  std::vector<double> probabilities;
+  size_t width;
+  size_t height;
+  nav_msgs::OccupancyGrid toOccupancyGrid() const;
+};
+
+nav_msgs::OccupancyGrid ProbabilityGrid::toOccupancyGrid() const {
+  // TODO implement conversion
+  //  1. set header, width, height
+  //  2. convert to int8 (0-100) range, see docs of OccupancyGrid
+  return nav_msgs::OccupancyGrid();
+}
+
+ProbabilityGrid cur_map(WIDTH * RESOLUTION, HEIGHT * RESOLUTION, PRIOR_PROB);
 
 int8_t& at(nav_msgs::OccupancyGrid& occupancy_grid, int x, int y)
 {
@@ -18,21 +36,13 @@ int8_t& at(nav_msgs::OccupancyGrid& occupancy_grid, int x, int y)
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  ROS_INFO(
-      "I heard: angle min %f angle max %f angle increment %f",
-      msg->angle_min,
-      msg->angle_max,
-      msg->angle_increment);
-}
+  // TODO prediction step
+  //  1. prediction on prob using 2D convolution
+  //  2. probs to log odds
 
-void reset_occupancy_grid(const size_t width, const size_t height, const int8_t init_value)
-{
-  occupancy_grid.header.frame_id = "map";
-  occupancy_grid.info.height = height;
-  occupancy_grid.info.width = width;
-
-  const size_t num_cells = width * height;
-  occupancy_grid.data = std::vector<int8_t>(num_cells, init_value);
+  // TODO update step 
+  //  1. update log odds through addition with ray casting
+  //  2. convert log odds to probabilities and publish ROS message based on that
 }
 
 int main(int argc, char** argv)
@@ -41,8 +51,6 @@ int main(int argc, char** argv)
 
   ros::NodeHandle nh_priv("~");
   ros::NodeHandle nh;
-
-  reset_occupancy_grid(WIDTH, HEIGHT, INIT_VALUE);
 
   ros::Publisher occ_pub = nh_priv.advertise<nav_msgs::OccupancyGrid>("occupancy", 1000);
   ros::Subscriber sub = nh.subscribe("scan", 1000, scanCallback);
